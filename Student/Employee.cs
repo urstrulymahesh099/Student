ab521691a547b8c2ff419bd94a5a97598e79d175
mago mahi <maheshhonnamurthy099@gmail.com>
2022-10-25 20:01:14 +0530

    [Mahesh]Add: second folder created

---
+++ Student/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class Employee
+{
+    public int id;
+    public String name;
+    public float salary;
+    public void insert(int i, String n, float s)
+    {
+        id = i;
+        name = n;
+        salary = s;
+    }
+    public void display()
+    {
+        Console.WriteLine(id + " " + name + " " + salary);
+    }
+}
+class TestEmployee
+{
+    public static void main (String[] args)
+    {
+        Employee e1 = new Employee();
+        Employee e2 = new Employee();
+        e1.insert(101, "Sonoo", 890000f);
+        e2.insert(102, "Mahesh", 490000f);
+        e1.display();
+        e2.display();
+
+    }
+}
