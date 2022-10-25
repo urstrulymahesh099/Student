37cd07a9677d8f3afc959496caa517a14ae7466d
mago mahi <maheshhonnamurthy099@gmail.com>
2022-10-25 19:46:27 +0530

    [Mahesh]Add:New folder created

--- Student/Program.cs
+++ Student/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;  
+   public class Student  
+    {  
+        int id;//data member (also instance variable)    
+        String name;//data member(also instance variable)    
+         
+    public static void Main(string[] args)  
+        {  
+            Student s1 = new Student();//creating an object of Student    
+            s1.id = 101;  
+            s1.name = "Mahesh naidu";  
+            Console.WriteLine(s1.id);  
+            Console.WriteLine(s1.name);  
+  
+        }  
+    }  
